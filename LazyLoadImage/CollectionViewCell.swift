4ab29c19c09ff4976442b7dbdb79bb3c218b0562
Ankita-kumari-2107 <ankita.kuymari@oyorooms.com>
2022-03-23 16:45:29 +0530

    changes

---
+++ LazyLoadImage/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  LazyLoadImage
+//
+//  Created by Ankita Kumari on 22/03/22.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ LazyLoadImage/CustomImageView.swift
@@ -0,0 +1,8 @@
+//
+//  CustomImageView.swift
+//  LazyLoadImage
+//
+//  Created by Ankita Kumari on 22/03/22.
+//
+
+import Foundation
